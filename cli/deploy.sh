eedc1589e145ccd220146f05a3379a460a0a3d56
Vladimir Yazykov <neizerth@gmail.com>
2025-10-20 18:20:32 +0300

    feat: deploy

---
+++ cli/deploy.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Create dist folder
+mkdir -p dist
+
+cp theme.json dist/theme.json
+# Create theme.zip archive from theme folder
+zip -r dist/theme.zip theme/
+
+echo "Created dist/theme.zip successfully!"
